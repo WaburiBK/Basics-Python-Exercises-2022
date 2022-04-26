b833dfc9ed5dd70a15e959109485306bf63fa03e
unknown <bwaburi007@gmail.com>
2022-04-26 06:38:24 +0300

    calculate area, circumference and diameter

--- Exercises/dict_tuples2.py
+++ Exercises/dict_tuples2.py
@@ -1,11 +1,14 @@
-stock_prices = {"info": [600, 630, 620], "ril": [1430, 1490, 1567], "mtl": [234, 180, 160]}
+import math
 
-op = input("Enter operation (add, print): ")
-if op == "add":
-    add()
-elif op == "print":
-    print()
-else:
-    quit()
+def circle_calc(radius):
+    area = math.pi * (radius ** 2)
+    circumference = math.pi * (2 * radius)
+    diameter = 2 * radius
+    return area, circumference, diameter
 
-def add():
+if __name__=="__main__":
+    radius = input("Enter a radius: ")
+    radius = float(radius)
+    area, circumference, diameter = circle_calc(radius)
+    print(f"Area {area}, Circumference {circumference}, Diameter {diameter}")
+
