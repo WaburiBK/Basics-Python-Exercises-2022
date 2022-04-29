625f11828161f95b3bc1f4f012417552a04dbac6
unknown <bwaburi007@gmail.com>
2022-04-29 08:25:51 +0300

    introduction to json

---
+++ address.py
@@ -0,0 +1,19 @@
+# create address book and write records
+# read the address book
+
+book = {}
+book["tom"] = {
+    "name": "tom",
+    "address": "1 Red Street, NY",
+    "phone": 256852856954
+}
+
+book["bob"] = {
+    "name": "bob",
+    "address": "1 Green Street, NY",
+    "phone": 256021745632
+}
+
+import json
+s = json.dumps(book)
+print(s)
